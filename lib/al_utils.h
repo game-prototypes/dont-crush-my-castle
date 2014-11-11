263ec75efcf3323c7801ccd13fa4de1e95d6d314
demiurgos <demiurgosoft@hotmail.com>
2014-11-11 23:25:50 +0100

    small changes in al_utils

--- lib/al_utils.h
+++ lib/al_utils.h
@@ -5,17 +5,17 @@
 //DESCRIPTION: Generic functions for drawing and handdling allegro bitmaps
 
 //resize bmp to given width and height (x and y), destroying the original bitmap
-void resize_bitmap(ALLEGRO_BITMAP *&bmp,int x,int y) {
+void resize_bitmap(ALLEGRO_BITMAP *&bitmap,unsigned int x,unsigned int y) {
     ALLEGRO_BITMAP *temp;
     ALLEGRO_BITMAP *disp=al_get_target_bitmap(); //actual display
-    int origx=al_get_bitmap_width(bmp);
-    int origy=al_get_bitmap_height(bmp);
+    int origx=al_get_bitmap_width(bitmap);
+    int origy=al_get_bitmap_height(bitmap);
     temp=al_create_bitmap(x,y); //the new bitmap with the given size
     al_set_target_bitmap(temp);
-    al_draw_scaled_bitmap(bmp,0,0, origx, origy,0,0, x,y,0);//draw the original bitmap to the new one (resized)
+    al_draw_scaled_bitmap(bitmap,0,0, origx, origy,0,0, x,y,0);//draw the original bitmap to the new one (resized)
     al_set_target_bitmap(disp); //returns the target to the display
     al_destroy_bitmap(bmp); //destroys the old bitmap
-    bmp=temp; //bmp now points to the resized bitmap
+    bitmap=temp; //bmp now points to the resized bitmap
 }
 
 //slice given bitmap in a vector of bitmaps, each birmap with given height and width (if ntiles>0, represents the max tiles to slice)
