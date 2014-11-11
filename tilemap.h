6cf2b11aac5e20f3cc34af3af6a6ce88cc02d5fe
demiurgos <demiurgosoft@hotmail.com>
2014-11-11 23:06:47 +0100

    Update tilemap
    Updated:
    * tileset.h
    * tilemap.h
    Erased:
    * debug_log.h
    * al_draw.h
    * tmxparser

--- tilemap.h
+++ tilemap.h
@@ -3,39 +3,91 @@
 //AUTHOR: Andrés Ortiz
 //VERSION: 0.1
 //DESCRIPTION: Generate,write,read and draw maps
-#include "lib/TmxParser/Tmx.h"
+
 #define empty_tile 0
 #define occupied_tile 1
 
 class tilemap {
 private:
-    int width;
-    int height;
-    vector< vector<int> > background; //background (grass,roads...)
+    //   unsigned int width; //map size (in tiles)
+    //   unsigned int height;
+
+    vector< vector<tile_id> > background; //background (grass,roads...)
     vector< vector<bool> > foreground; //foreground (occupied tiles)
-    tileset *tiles;
+    tileset* tiles; //pointer to tileset
 public:
     tilemap() {
-        width=0;
-        height=0;
     }
-
 
 
     //Loads from tmx file
-    void loadtmx(string filename) {
-        Tmx::Map map;
-        map.ParseFile(filename);
-        if(map.HasError()==false) {
-            int layers=map.GetNumLayers();
-            //TODO
-        }
+    /*   void loadtmx(string filename) {
+           Tmx::Map map;
+           map.ParseFile(filename);
+           if(map.HasError()==false) {
+               //   int layers=map.GetNumLayers(); //we will use only first layer
+               int width=map.GetWidth();
+               int height=map.GetHeight();
+               init_matrix(width,height);
+               const Tmx::Layer *back=map.GetLayer(0); //get the background layer
+               load_background(back,width,height);
+               tiles.clear();
+               const vector< Tmx::Tileset * > vts=map.GetTilesets();
+               if(vts.size()==0) debug_log::report("Not tileset",err,true,true,false);
+               else if(vts.size()>1) debug_log::report("more than one tilesey",warning,true,false,false);
+               for(int i=0; i<vts.size(); i++) {
+                   tiles.add_tileset(vts[i]);
+               }
+               check();
+           }
+       }*/
+    int get_height() {
+        return background.size();
     }
+    int get_width() {
+        if(background.size()>0) return background[0].size();
+        else return 0;
+    }
+    void draw_tilemap() {
+        int height=get_height();
+        int width=get_width();
+        float x=0,y=0;
+        int tilew=tiles->get_tile_width();
+        int tileh=tiles->get_tile_height();
+        for(int i=0; i<height; i++) {
+            x=0;
+            for(int j=0; j<width; j++) {
+                draw_tile(background[i][j],x,y);
+                x+=tilew;
+            }
+            y+=tileh;
+        }
+
 
 private:
-    void check() {
+        void check() {
+            if(background.size()!=foreground.size()) debug_log::report("map matrix don't match(height)",err,true,true,false);
+            if(background.size()>0 && foreground.size()>0 && background[0].size()!=foreground[0].size()) debug_log::report("map matrix don't match(width)",err,true,true,false);
+        }
 
-    }
+        void init_matrix(int width,int height) {
+            foreground.clear();
+            background.clear();
+            foreground.resize(height);
+            background.resize(height);
+            vector<bool> v(width,empty_tile);
+            for(int i=0; i<height; i++) {
+                foreground.push_back(v);
+                background.resize(width);
+            }
+        }
+        /*   void load_background(const Tmx::Layer *lay,int width,int height) {
+               for(int i=0; i<height; i++) {
+                   for(int j=0; j<width; j++) {
+                       background[i][j]=lay->GetTileTilesetIndex(i,j);
+                   }
+               }
+           }*/
 
 
-};
+    };
--- tileset.h
+++ tileset.h
@@ -4,8 +4,6 @@
 //VERSION: 0.1
 //DESCRIPTION: Information about a set of tiles to be used on maps
 
-#include "lib/al_draw.h"
-
 enum tile_type {blocked,road,ground,special,null_tile};
 const string tmx_tiletype_property="tiletype"; //defines the name of the property used on tmx for tiletype
 /*tile_type:
@@ -16,94 +14,123 @@ ground: free for construction
 class tile {
 public:
     tile_type type;
-    int id;
 private:
     ALLEGRO_BITMAP *bitmap;
 public:
-    tile(int id,tile_type type,ALLEGRO_BITMAP *bitmap) {
+    tile() {
+        type=null_tile;
+    }
+    tile(tile_type type,ALLEGRO_BITMAP *bitmap) {
         this->type=type;
         this->bitmap=bitmap;
-        this->id=id;
     }
 
     ~tile() {
         al_destroy_bitmap(bitmap);
     }
 
-    void draw_tile(float x,float y) {
+    void draw(float x,float y) {
         al_draw_bitmap(bitmap,x,y,0);
+    }
+    void draw_resized(float x,float y,unsigned int width,unsigned int height) {
+        if(width==0 || height==0) debug_log::report("tile size=0",err,true,false,false);
+        else
+            al_draw_scaled_bitmap(bitmap,0.0,0.0,al_get_bitmap_width(bitmap),al_get_bitmap_height(bitmap),x,y,width,height);
+
     }
 };
 
 
 
-
+#define tile_id int
 class tileset {
 private:
-    vector<tile> tile_list;
+    map<tile_id,tile> tile_list;
     string name;
     int height;
-    int widht; //each tile height and width
+    int width; //each tile height and width
 
 public:
     tileset() {
         height=width=0;
     }
+    /*   tileset(const Tmx::Tileset *ts) {
+           height=ts->GetTileHeight();
+           width=ts->GetTileWidth();
+           name=ts->GetName();
+           add_tileset(ts);
+       }*/
     //gets a tile with given id
-    tile get_tile(unsigned int id) {
-        return tile_list[id];
+    tile get_tile(tile_id id) {
+        tile ret;
+        map<tile_id,tile>::const_iterator it;
+        it=tile_list.find(id);
+        if(it!=tile_list.end()) ret=it->second;
+        return ret;
     }
-    pair<int,int> get_tile_size() {
-        return make_pair(width,height);
+    int get_tile_width() {
+        return width;
+    }
+    int get_tile_height() {
+        return height;
+    }
+    void clear() {
+        name.clear();
+        height=width=0;
+        tile_list.clear();
+    }
+    void draw_tile(tile_id id,float x,float y) {
+        get_tile(id).draw(x,y);
+    }
+    void draw_scaled_tile(tile_id id,float x,float y,unsigned int width,unsigned int height) {
+        get_tile(id).draw_scaled(x,y,width,height);
     }
 
-    void loadtmx(const string &name) {
-        Tmx::Tileset ts;
-        ts.ParseFile(filename);
-        if(ts.HasError()==false) {
-            height=ts.GetTileHeight();
-            width=ts.GetTileWidth();
-            name=ts.GetName();
-
-            vector<Tmx::Tile*> tiles=ts.GetTiles();
-            int siz=tiles.size();
-
-            string imgpath=ts.GetImage()->GetSource(); //image source path
-            ALLEGRO_BITMAP *bitmap;
-            bitmap=al_load_bitmap(imgpath.c_str()); //loads bitmap
-            vector<ALLEGRO_BITMAP*> tileset_images=slice_bitmap(bitmap,width,height,siz);//slice bitmap
-            al_destroy_bitmap(bitmap);
-
-            //loads all the tiles
-            tile_list.reserve(siz));
-            for(int i=0; i<siz; i++) {
-            ALLEGRO_BITMAP* bmp=tileset_images[i]; //tile image
-                int id=tiles[i]->GetId(); //tile id
-                Tmx::PropertySet ps=tiles[i]->GetProperties();//tile properties
-                string tt=ps.GetStringProperty(tmx_tiletype_property);
-                tile_type t;
-                if(tt=="blocked") t=blocked;
-                else if(tt=="road") t=road;
-                else if(tt=="ground") t=ground;
-                else if(tt=="special") t=special;
-                else t=null_tile;
-                if(t==null_tile) debug_log::report("null tile parsed",err,true,false);
-                tile_list.push_back(tile(id,t,bmp));
-            }
-        }
-        check();
-    }
+    //adds a tileset to the actual tileset (resizing bitmaps), name and sizes will remain unchanged
+    //tiles with same id will not be inserted
+    /*  void add_tmx_tileset(const Tmx::Tileset *ts) {
+          if(name.empty()==true) name=ts->GetName();
+          if(height==0 || width==0) {
+              width=ts->GetTileWidth();
+              height=ts->GetTileHeight();
+          }
+          else if(ts->GetTileWidth()!=width || ts->GetTileHeight()!=height) debug_log::report("not matching sizes on tileset,will resize bitmaps",warning,false,true,false);
+          vector<Tmx::Tile *> tiles=ts->GetTiles();
+          int siz=tiles.size();
+          string imgpath=(ts->GetImage())->GetSource(); //image source path
+          ALLEGRO_BITMAP *bitmap;
+          bitmap=al_load_bitmap(imgpath.c_str()); //loads bitmap
+          vector<ALLEGRO_BITMAP *> tileset_images=slice_bitmap(bitmap,width,height,siz); //slice bitmap
+          al_destroy_bitmap(bitmap);
+          //loads the tiles
+          for(int i=0; i<siz; i++) {
+              ALLEGRO_BITMAP *bmp=tileset_images[i]; //tile image
+              int id=tiles[i]->GetId(); //tile id
+              Tmx::PropertySet ps=tiles[i]->GetProperties();//tile properties
+              string tt=ps.GetStringProperty(tmx_tiletype_property);
+              tile_type t;
+              if(tt=="blocked") t=blocked;
+              else if(tt=="road") t=road;
+              else if(tt=="ground") t=ground;
+              else if(tt=="special") t=special;
+              else t=null_tile;
+              if(t==null_tile) debug_log::report("null tile parsed",err,true,false);
+              if(add_tile(id,t,bmp)==false) {
+                  al_destroy_bitmap(bmp);
+              }
+          }
+          check();
+      }*/
 
 
 private:
-    unsigned int add_tile(int id,tile_type type,ALLEGRO_BITMAP *bitmap) {
+    //return true if inserted, false if already exists
+    bool add_tile(tile_id id,tile_type type,ALLEGRO_BITMAP *bitmap) {
         if(al_get_bitmap_height(bitmap)!=height || al_get_bitmap_width(bitmap)!=width)
-            resize_bitmap(bitmap,widht,height);
-        tile_list.push_back(tile(id,type,bitmap));
-        return tile_list.size()-1;
+            resize_bitmap(bitmap,width,height);
+        return  tile_list.insert(make_pair(id,tile(type,bitmap))).second;
     }
 
     void check() {
-
     }
 };
